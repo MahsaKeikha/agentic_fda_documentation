76c5378d394ac239b028675e6d3f966ac89a452a
MahsaKeikha <mahsa.keikha@gmail.com>
2026-08-21 23:08:09 -0700

    Make F54 smoke run explicit

--- run.py
+++ run.py
@@ -1,2 +1,5 @@
 from orchestration.orchestrator import run_workflow
-if __name__=="__main__":print(run_workflow({"documents":[],"requirements":[],"human_approved":False}))
+
+
+if __name__ == "__main__":
+    print(run_workflow({}))
